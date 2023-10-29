a546e46e5dedbb15e9f2bdd25a9a15f49e2789b4
Selim Enes <sellim81@gmail.com>
2023-10-29 16:05:22 +0300

    responsive changes

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -6,26 +6,33 @@ type Props = {};
 
 const AboutPage = (props: Props) => {
   return (
-    <div className="flex flex-col h-full relative">
-      <div className="flex flex:col flex-1 md:pl-36 md:py-36 p-10 overflow-x-hidden ">
-        <div className="md:w-1/2 w-full">
-          <h1 className="text-[#161519] text-3xl md:text-6xl font-semibold mb-4 md:mb-10">
+    <div className="flex flex-col h-full relative overflow-x-hidden">
+      <div className="flex flex:col flex-1 lg:pl-36 lg:py-36 p-10">
+        <div className="lg:w-1/4 w-full">
+          <h1 className="text-[#161519] text-3xl lg:text-6xl font-semibold mb-4 lg:mb-10">
             ÖmerFarukErdoğan
           </h1>
-          <p className="text-[#161519] mb-4 leading-relaxed text-sm md:text-normal">
+          <p className="text-[#161519] mb-4 leading-relaxed text-sm lg:text-normal">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
             eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis
             ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas
             accumsan lacus vel facilisis. Vestibulum luctus ex ac ipsum auctor
             convallis.
           </p>
-          <p className="text-[#161519] leading-relaxed text-sm md:text-normal">
+          <p className="text-[#161519] leading-relaxed text-sm lg:text-normal">
             Morbi sodales congue odio sed rhoncus. Proin odio quam, suscipit nec
             commodo vestibulum, malesuada a erat. Mauris dignissim mattis
             iaculis. Vestibulum vestibulum tincidunt lectus at Quis ipsum
             suspendisse ultrices gravida. Risus commodo viverra
           </p>
         </div>
+        <Image
+          src={"/images/about-me-pc-camera.png"}
+          width={2200}
+          height={1100}
+          alt="pc camera"
+          className="absolute right-[-15%] md:bottom-[20%] bottom-[38%] w-[80%] md:w-[100%] object-contain"
+        />
       </div>
       <AboutSkills />
     </div>
--- components/AboutSkills.tsx
+++ components/AboutSkills.tsx
@@ -16,14 +16,14 @@ const AboutSkills = (props: Props) => {
   ];
   return (
     <div className="gradientBg w-full">
-      <div className=" text-white p-6 md:p-24">
-        <h2 className="text-[#F7F7F7] text-5xl md:text-7xl font-semibold">
+      <div className=" text-white p-6 lg:p-24">
+        <h2 className="text-[#F7F7F7] text-5xl lg:text-7xl font-semibold">
           Neler <br /> Yapıyorum?
         </h2>
-        <div className="mt-8 grid grid-cols-1 md:grid-cols-2 md:w-1/2">
+        <div className="mt-8 grid grid-cols-1 md:grid-cols-2 lg:w-1/2">
           {skills.map((skill, index) => (
             <p
-              className="text-[#F7F7F7] md:text-2xl text-lg flex items-center gap-2"
+              className="text-[#F7F7F7] lg:text-2xl text-lg flex items-center gap-2"
               key={index}
             >
               <ChevronRight className="text-[#6100A5]" /> {skill}
